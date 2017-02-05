00be3cc4f9cab6d33e2a0835a0a4d2dd6f9e12e1
Derek Kaknes <derek.kaknes@gmail.com>
2017-02-04 19:55:24 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :phone_number, format: { with: /\d{10}/,
+                                     message: "%{value} must be 10 digit number"}
 end
