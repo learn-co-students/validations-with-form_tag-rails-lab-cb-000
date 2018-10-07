2537bee74277446ec193aa2bb0dc9648ca901d53
Elizabeth J <elizabeth@wondertwinsventures.com>
2018-10-07 12:20:30 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,23 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+
   end
 
   def update
     @post = Post.find(params[:id])
 
     @post.update(post_params)
+    
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+     else
+       render :edit
+     end
 
-    redirect_to post_path(@post)
   end
+    
 
   private
 
---
+++ vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/lizjen/Development/code/validations-with-form_tag-rails-lab-cb-000/vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
